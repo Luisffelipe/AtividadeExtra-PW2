ebeee1dc92fa1a537924a78550939963dd856a38
Luis Felipe M <luis.felippe@academico.ifpb.edu.br>
2023-05-26 20:07:12 -0300

    feat: Criando componentes avatar e header

---
+++ src/components/Avatar/index.jsx
@@ -0,0 +1,9 @@
+import styles from './index.module.css';
+
+export function Avatar() {
+    return (
+            <div className={styles.profile}>
+                <img src="https://avatars.githubusercontent.com/u/87948087?v=4" />
+            </div>
+    )
+}
